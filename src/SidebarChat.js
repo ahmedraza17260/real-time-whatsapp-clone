c9cc6440a74b269a74a9d5aa05cc326cd6f3639b
ahmedraza17260 <ahmedraza17260gmail.com>
2020-09-14 19:28:19 +0500

    host website

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -19,7 +19,7 @@ function SidebarChat({ id, name, addNewChart }) {
           setMessages(snapshot.docs.map((doc) => doc.data()))
         );
     }
-  }, []);
+  }, [id]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
